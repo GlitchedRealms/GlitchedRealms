e5f8b72309405c56425449c1413f707ffccd2d40
Joshua Anand <joshua.anand316@gmail.com>
2025-05-12 20:06:59 -0400

    added authentication with firebase via Pyrebase4

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, session, redirect, url_for, request
 from flask_socketio import SocketIO, disconnect
 import os
+import pyrebase
+from dotenv import load_dotenv
 from docker_information import docker_manager
 from socket_routes import register_socket_routes  # <- changed name!
 
@@ -8,6 +10,21 @@ app = Flask(__name__)
 app.secret_key = os.urandom(24)
 sio = SocketIO(app, cors_allowed_origins="*", manage_session=False)
 docker_mgr = docker_manager()
+load_dotenv(".env")
+
+config = {
+    "apiKey": os.getenv("API_KEY"),
+    "authDomain": os.getenv("AUTH_DOMAIN"),
+    "projectId": os.getenv("PROJECT_ID"),
+    "storageBucket": os.getenv("STORAGE_BUCKET"),
+    "messagingSenderId": os.getenv("MESSAGING_SENDER_ID"),
+    "appId":os.getenv("APP_ID"),
+    "measurementId":os.getenv("MEASUREMENT_ID"),
+    "databaseURL":"n/a",
+};
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
 
 register_socket_routes(sio, docker_mgr)
 
@@ -24,15 +41,37 @@ def login():
         return render_template("login.html")
     elif request.method == 'POST':
         user_id = request.form.get('username')
-        if user_id:
-            session['user_id'] = user_id
+        user_pwd = request.form.get('password')
+        if 'user_id' in session:
             return redirect(url_for('index'))
+        else:
+            try:
+                user = auth.sign_in_with_email_and_password(user_id, user_pwd)
+                session['user_id'] = user_id
+                return redirect(url_for('index'))
+            except:
+                return "password incorrect"
+
         return redirect(url_for('index'))
 
 @app.route('/logout')
 def logout():
     session.pop('user_id', None)
     return redirect(url_for('index'))
+
+@app.route('/sign_up',methods=['GET','POST'])
+def sign_up():
+    if request.method == 'GET':
+        return render_template("signup.html")
+    elif request.method == 'POST':
+        user_id = request.form.get('username')
+        user_pwd = request.form.get('password')
+        try:
+            auth.create_user_with_email_and_password(user_id, user_pwd)
+        except Exception as e:
+            return f"something went wrong :(, please try again later. Here's the error: {e}"
+
+        return redirect(url_for('login'))
 
 @app.route('/terminal/<container_name>')
 def terminal(container_name):
